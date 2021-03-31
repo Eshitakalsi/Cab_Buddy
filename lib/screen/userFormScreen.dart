f3102c345982196e12f2330769d72c8f79afac05
harshtekriwal <45792325+harshtekriwal@users.noreply.github.com>
2021-03-31 19:30:15 +0530

    Complete Signup page

--- lib/screen/userFormScreen.dart
+++ lib/screen/userFormScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:cab_buddy/Pickers/userImagePicker.dart';
+import 'package:cab_buddy/animation/FadeAnimation.dart';
 import 'package:cab_buddy/models/loggedInUserInfo.dart';
 import 'package:cab_buddy/screen/homePage.dart';
 import 'package:cab_buddy/screen/profilePage.dart';
@@ -146,139 +147,195 @@ class _UserFormScreenState extends State<UserFormScreen> {
       }
     }
 
-    Widget userInfoForm = Scaffold(
-      body: Center(
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-          elevation: 5,
-          margin: EdgeInsets.all(25),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    UserImagePicker(_pickedImage),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'FirstName'),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please Enter your First Name';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _firstname = value;
-                      },
-                      key: ValueKey('firstName'),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: 'LastName'),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Please Enter your Last Name';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        _lastname = value;
-                      },
-                      key: ValueKey('lastName'),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    DropdownButton<String>(
-                      hint: _year == null
-                          ? Text('Select Your Year')
-                          : Text(
-                              _year,
-                            ),
-                      items: _years.map((String val) {
-                        return new DropdownMenuItem<String>(
-                          value: val,
-                          child: new Text(val),
-                        );
-                      }).toList(),
-                      onChanged: (newVal) {
-                        _year = newVal;
-                        this.setState(() {});
-                      },
-                    ),
-                    DropdownButton<String>(
-                      hint: _batch == null
-                          ? Text('Select Your Batch')
-                          : Text(
-                              _batch,
-                            ),
-                      items: _batches.map((String val) {
-                        return new DropdownMenuItem<String>(
-                          value: val,
-                          child: new Text(val),
-                        );
-                      }).toList(),
-                      onChanged: (newVal) {
-                        _batch = newVal;
-                        this.setState(() {});
-                      },
-                    ),
-                    DropdownButton<String>(
-                      hint: _gender == null
-                          ? Text('Select Your Gender')
-                          : Text(
-                              _gender,
-                            ),
-                      items: _genders.map((String val) {
-                        return new DropdownMenuItem<String>(
-                          value: val,
-                          child: new Text(val),
-                        );
-                      }).toList(),
-                      onChanged: (newVal) {
-                        _gender = newVal;
-                        this.setState(() {});
-                      },
-                    ),
-                    DropdownButton<String>(
-                      hint: _sector == null
-                          ? Text('Select Sector')
-                          : Text(
-                              _sector,
-                            ),
-                      items: _sectors.map((String val) {
-                        return new DropdownMenuItem<String>(
-                          value: val,
-                          child: new Text(val),
-                        );
-                      }).toList(),
-                      onChanged: (newVal) {
-                        _sector = newVal;
-                        this.setState(() {});
-                      },
-                    ),
-                    _isLoading
-                        ? CircularProgressIndicator()
-                        : RaisedButton(
-                            color: Colors.blueGrey[100],
-                            child: Text('Submit'),
-                            onPressed: () {
-                              _trySubmit();
-                            },
-                          ),
-                  ],
-                ),
+    Widget userForm = Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: MediaQuery.of(context).size.height - 50,
+          width: double.infinity,
+          child: Column(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-            ),
+              Column(
+                children: <Widget>[
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        UserImagePicker(_pickedImage),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'FirstName'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please Enter your First Name';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _firstname = value;
+                          },
+                          key: ValueKey('firstName'),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: 'LastName'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please Enter your Last Name';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _lastname = value;
+                          },
+                          key: ValueKey('lastName'),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        DropdownButton<String>(
+                          hint: _year == null
+                              ? Text('Select Your Year')
+                              : Text(
+                                  _year,
+                                ),
+                          items: _years.map((String val) {
+                            return new DropdownMenuItem<String>(
+                              value: val,
+                              child: new Text(val),
+                            );
+                          }).toList(),
+                          onChanged: (newVal) {
+                            _year = newVal;
+                            this.setState(() {});
+                          },
+                        ),
+                        DropdownButton<String>(
+                          hint: _batch == null
+                              ? Text('Select Your Batch')
+                              : Text(
+                                  _batch,
+                                ),
+                          items: _batches.map((String val) {
+                            return new DropdownMenuItem<String>(
+                              value: val,
+                              child: new Text(val),
+                            );
+                          }).toList(),
+                          onChanged: (newVal) {
+                            _batch = newVal;
+                            this.setState(() {});
+                          },
+                        ),
+                        DropdownButton<String>(
+                          hint: _gender == null
+                              ? Text('Select Your Gender')
+                              : Text(
+                                  _gender,
+                                ),
+                          items: _genders.map((String val) {
+                            return new DropdownMenuItem<String>(
+                              value: val,
+                              child: new Text(val),
+                            );
+                          }).toList(),
+                          onChanged: (newVal) {
+                            _gender = newVal;
+                            this.setState(() {});
+                          },
+                        ),
+                        DropdownButton<String>(
+                          hint: _sector == null
+                              ? Text('Select Sector')
+                              : Text(
+                                  _sector,
+                                ),
+                          items: _sectors.map((String val) {
+                            return new DropdownMenuItem<String>(
+                              value: val,
+                              child: new Text(val),
+                            );
+                          }).toList(),
+                          onChanged: (newVal) {
+                            _sector = newVal;
+                            this.setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              ),
+              _isLoading
+                  ? Container(
+                      height: MediaQuery.of(context).size.height / 7,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/car.gif'),
+                        ),
+                      ),
+                    )
+                  : FadeAnimation(
+                      1.5,
+                      Container(
+                        padding: EdgeInsets.only(top: 3, left: 3),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border(
+                              bottom: BorderSide(color: Colors.black),
+                              top: BorderSide(color: Colors.black),
+                              left: BorderSide(color: Colors.black),
+                              right: BorderSide(color: Colors.black),
+                            )),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            _trySubmit();
+                          },
+                          color: Colors.greenAccent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 18),
+                          ),
+                        ),
+                      )),
+            ],
           ),
         ),
       ),
     );
+
     if (_hasUserDataAlready) {
       LoggedInUserInfo.id = widget._uid;
       LoggedInUserInfo.name = _firstname + " " + _lastname;
@@ -291,7 +348,36 @@ class _UserFormScreenState extends State<UserFormScreen> {
 
       return ProfilePage();
     } else {
-      return userInfoForm;
+      return userForm;
     }
+  }
+
+  Widget makeInput({label, obscureText = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(
+              fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        TextField(
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey[400])),
+          ),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+      ],
+    );
   }
 }
